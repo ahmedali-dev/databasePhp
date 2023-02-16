73c5272202cab9270d3715d25beb2efbbd435372
ahmedali-dev <mohamedali850100@gmail.com>
2023-02-16 23:02:08 +0200

    add config.php

---
+++ Config.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/**
+ * host:=> locathost
+ * dbname:=> database name
+ * username:=> database username (root)
+ * password:=> database password ('')
+ */
+
+
+define("host", "localhost");
+define("dbname", "test");
+define("username", "root");
+define("password", "");
